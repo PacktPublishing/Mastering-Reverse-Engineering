f624d4b4c1766c31bd232c899175f9650331a6cd
reginald <b0yb4w4n9@gmail.com>
2020-11-26 22:02:09 +0800

    updated server code

--- ch6/server.c
+++ ch6/server.c
@@ -8,10 +8,11 @@
 int main(int argc, char *argv[])
 {
     int listenfd = 0, connfd = 0, n;
-    struct sockaddr_in serv_addr;
-    struct sockaddr_in ctl_addr;
+    struct sockaddr_in serv_addr = { 0 };
+    struct sockaddr_in ctl_addr = { 0 };
     int addrlen;
     char sendBuff[1025];
+    char recvBuff[1025];
 
     listenfd = socket(AF_INET, SOCK_STREAM, 0);
     memset(&serv_addr, '0', sizeof(serv_addr));
@@ -26,13 +27,16 @@ int main(int argc, char *argv[])
     listen(listenfd, 10); 
     
     printf("Genie is waiting for connections to port 9999.\n");
+    snprintf(sendBuff, sizeof(sendBuff), "You have connected to the Genie. Nothing to see here.\n\n");
     while(1)
     {
+        memset(&ctl_addr, 0, sizeof(ctl_addr));
+        addrlen = sizeof(ctl_addr);
         connfd = accept(listenfd, (struct sockaddr*)&ctl_addr, &addrlen);
+        memset(&recvBuff, 0, 1024);
+        read(connfd, recvBuff, 1024);
         printf("%s has connected.\n", inet_ntoa(ctl_addr.sin_addr));
-
-        snprintf(sendBuff, sizeof(sendBuff), "You have connected to the Genie. Nothing to see here.\n\n");
-        write(connfd, sendBuff, strlen(sendBuff)); 
+        send(connfd, sendBuff, strlen(sendBuff),0); 
 
         close(connfd);
         sleep(1);
